a495b6fa491cd9f37d731d3db8ee15eac6f35a1b
Prashant <prashant.barca@gmail.com>
2015-10-03 22:07:18 -0400

    changed gemfile lock

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004014309) do
+ActiveRecord::Schema.define(version: 20151004014332) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -46,6 +46,19 @@ ActiveRecord::Schema.define(version: 20151004014309) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "events", force: true do |t|
+    t.decimal  "latitude"
+    t.decimal  "longitude"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.date     "date"
+    t.boolean  "alcohol"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -62,6 +75,10 @@ ActiveRecord::Schema.define(version: 20151004014309) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.boolean  "dartmouth"
+    t.integer  "school"
+    t.decimal  "latitude"
+    t.decimal  "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
